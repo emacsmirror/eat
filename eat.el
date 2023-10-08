db63d46739b7ae91717783dd24cfb01e30dc6510
Akib Azmain Turja <akib@disroot.org>
2023-10-09 00:25:02 +0600

    ; Release version 0.9.1
    * eat.el:
    * eat.texi:
    Bump version.
    * ChangeLog: Update.

--- eat.el
+++ eat.el
@@ -4,7 +4,7 @@
 
 ;; Author: Akib Azmain Turja <akib@disroot.org>
 ;; Created: 2022-08-15
-;; Version: 0.9
+;; Version: 0.9.1
 ;; Package-Requires: ((emacs "26.1") (compat "29.1"))
 ;; Keywords: terminals processes
 ;; Homepage: https://codeberg.org/akib/emacs-eat
